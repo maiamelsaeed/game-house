848507d3e90411af3c52f8a835688e1e9596ef19
ammar <amaralgalady@gmail.com>
2024-05-24 23:17:07 +0300

    Refactoring Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,27 +12,26 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GamesCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GamesCardContainer>
-          ))}
-        {data.map((game) => (
-          <GamesCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GamesCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GamesCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GamesCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GamesCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -30,35 +30,33 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   }
 
   return (
-    <>
-      <List>
-        <Heading fontSize="2xl" marginBottom={3}>
-          Genres
-        </Heading>
-        {data.map((genre) => (
-          <ListItem key={genre.id} paddingY="5px">
-            <HStack>
-              <Image
-                boxSize="32px"
-                borderRadius={8}
-                objectFit="cover"
-                src={getCroppedImageUrl(genre.image_background)}
-              />
-              <Button
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-                onClick={() => onSelectGenre(genre)}
-                fontSize="lg"
-                variant="link"
-                whiteSpace="normal"
-                textAlign="left"
-              >
-                {genre.name}
-              </Button>
-            </HStack>
-          </ListItem>
-        ))}
-      </List>
-    </>
+    <List>
+      <Heading fontSize="2xl" marginTop={2} marginBottom={3}>
+        Genres
+      </Heading>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              objectFit="cover"
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+              whiteSpace="normal"
+              textAlign="left"
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
