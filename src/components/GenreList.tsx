87552b2d9307edfde0d65d508994f9c1c1a80832
Mariam Alsaeed <mariam.elgalady1@gmail.com>
2024-06-11 03:42:28 +0300

    fetch games with react query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -34,7 +34,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
       <Heading fontSize="2xl" marginTop={2} marginBottom={3}>
         Genres
       </Heading>
-      {data.map((genre) => (
+      {data?.results.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
